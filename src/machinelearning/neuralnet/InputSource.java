85d116768d382d7126b9368b0830c00bb5498268
Akarsh Kumar <akarshkumar0101@gmail.com>
2019-07-29 23:43:21 -0500

    Moved features to LibAK-CAS, ML, and Evolution

--- src/machinelearning/neuralnet/InputSource.java
+++
@@ -1,8 +0,0 @@
-package machinelearning.neuralnet;
-
-public interface InputSource {
-
-	public abstract double getInput(int inputIndex);
-
-	public static final InputSource EMPTY_INPUT_SOURCE = inputIndex -> 0.0;
-}
--- src/test/Test.java
+++ src/test/Test.java
@@ -3,31 +3,33 @@ package test;
 import java.util.Map;
 
 import math.Factorization;
-import math.cas.CAS;
-import math.cas.Expression;
+
 
 public class Test {
 
-	public static void main(String[] args) {
-
+    public static void main(String[] args) {
+        System.out.println("hey");
 	}
 
-	public static void randomStuff() {
-		Map<Integer, Integer> factors = Factorization.factorize(9708131);
-		for (int key : factors.keySet()) {
-			System.out.println(key + "^" + factors.get(key));
-		}
-	}
+    public static void randomStuff() {
+        Map<Integer, Integer> factors = Factorization.factorize(9708131);
+        for (int key : factors.keySet()) {
+            System.out.println(key + "^" + factors.get(key));
+        }
+    }
 
-	public static void testCAS() {
-		// System.out.println(Math.PI * Math.sin(Math.pow(2.3, Math.tan(2.3))));
+    /*
+    public static void testCAS() {
+        // System.out.println(Math.PI * Math.sin(Math.pow(2.3, Math.tan(2.3))));
 
-		CAS cas = new CAS();
-		cas.registerVariable("x");
-		cas.registerVariable("y");
+        CAS cas = new CAS();
+        cas.registerVariable("x");
+        cas.registerVariable("y");
 
-		Expression exp = new Expression(cas, "(9*x+x*2)*y");
-		System.out.println(exp.getRoot().partialWithRespectTo(cas.getVariable("x")).consolidate());
+        Expression exp = new Expression(cas, "(9*x+x*2)*y");
+        System.out.println(exp.getRoot().partialWithRespectTo(cas.getVariable("x")).consolidate());
 
-	}
+    }
+    */
+
 }
